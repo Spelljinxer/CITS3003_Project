eed15c6cb696575b78e9f32e73c09b9b260bd295
Spelljinxer <35420739+Spelljinxer@users.noreply.github.com>
2022-04-18 17:48:24 +0800

    Task G - moving light to fragment

--- vStart.glsl
+++ vStart.glsl
@@ -1,61 +1,27 @@
 attribute vec3 vPosition;
+varying vec3 position;
+
 attribute vec3 vNormal;
+varying vec3 normal;
+
 attribute vec2 vTexCoord;
-
 varying vec2 texCoord;
-varying vec4 color;
 
-uniform vec3 AmbientProduct, DiffuseProduct, SpecularProduct;
 uniform mat4 ModelView;
 uniform mat4 Projection;
-uniform vec4 LightPosition;
-uniform float Shininess;
 
 void main()
 {
+
     vec4 vpos = vec4(vPosition, 1.0);
 
     // Transform vertex position into eye coordinates
     vec3 pos = (ModelView * vpos).xyz;
-
-
-    // The vector to the light from the vertex    
-    vec3 Lvec = LightPosition.xyz - pos;
-
-    // Unit direction vectors for Blinn-Phong shading calculation
-    vec3 L = normalize( Lvec );   // Direction to the light source
-    vec3 E = normalize( -pos );   // Direction to the eye/camera
-    vec3 H = normalize( L + E );  // Halfway vector
-
-    // Transform vertex normal into eye coordinates (assumes scaling
-    // is uniform across dimensions)
-    vec3 N = normalize( (ModelView*vec4(vNormal, 0.0)).xyz );
-
-    // Compute terms in the illumination equation
-    vec3 ambient = AmbientProduct;
-
-    float Kd = max( dot(L, N), 0.0 );
-    vec3  diffuse = Kd*DiffuseProduct;
-
-    float Ks = pow( max(dot(N, H), 0.0), Shininess );
-    vec3  specular = Ks * SpecularProduct;
     
-    if (dot(L, N) < 0.0 ) {
-	specular = vec3(0.0, 0.0, 0.0);
-    } 
-
-    //PART F 
-    float lightDist = 0.1 + length(Lvec);
-    float light = 1.0/(1.0 + length(Lvec) + lightDist);
-    //ENDOF PART F
-
-
-    // globalAmbient is independent of distance from the light source
-    vec3 globalAmbient = vec3(0.1, 0.1, 0.1);
-    //color.rgb = globalAmbient  + ambient + diffuse + specular;
-    color.rgb = globalAmbient + ((ambient + diffuse) / lightDist) + specular + light;
-    color.a = 1.0;
-
     gl_Position = Projection * ModelView * vpos;
+
+    position = pos;
+    normal = vNormal;
     texCoord = vTexCoord;
+
 }
