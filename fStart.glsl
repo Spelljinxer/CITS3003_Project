6c03795bb90845bade6f0355d8c3fa25ea7236e2
NEDEN249 <61109926+NEDEN249@users.noreply.github.com>
2022-05-23 11:16:47 +0800

    made a spotl- wait...thats not a spotlight
    its a mf flashlight

--- fStart.glsl
+++ fStart.glsl
@@ -26,6 +26,8 @@ uniform vec4 LightPosition3;
 uniform float LightBrightness3;
 uniform vec3 LightColor3;
 uniform vec4 LightLocation3;
+uniform float pitch;
+uniform float yaw;
 
 void main()
 {
@@ -35,7 +37,10 @@ void main()
     vec3 Lvec2 = LightPosition2.xyz;
    
     vec3 Lvec3 = LightPosition3.xyz - position;
-
+    vec3 direct;
+    direct.x = cos(radians(yaw))*cos(radians(pitch));
+    direct.y = sin(radians(pitch));
+    direct.z = sin(radians(yaw))*cos(radians(pitch));
     // Unit direction vectors for Blinn-Phong shading calculation
     vec3 L = normalize( Lvec );   // Direction to the light source
     vec3 L2 = normalize( Lvec2 ); 
@@ -44,29 +49,27 @@ void main()
     vec3 H = normalize( L + E );  // Halfway vector
     vec3 H2 = normalize( L2 + E);
     vec3 H3 = normalize( L3 + E );
-
     // Transform vertex normal into eye coordinates (assumes scaling
     // is uniform across dimensions)
     vec3 N = normalize( (ModelView*vec4(normal, 0.0)).xyz );
-
     // Compute terms in the illumination equation
     vec3 ambient = (LightColor * LightBrightness) + AmbientProduct;
     vec3 ambient2 = (LightColor2 * LightBrightness2) * AmbientProduct;
     vec3 ambient3 = (LightColor3 * LightBrightness3) * AmbientProduct;
-
+    float theta = dot(L3, normalize(-direct));
 
     float Kd = max( dot(L, N), 0.0 );
     vec3  diffuse = Kd * (LightColor * LightBrightness) * DiffuseProduct;
     float Kd2 = max(dot(L2, N), 0.0);
     vec3  diffuse2 = Kd2 * (LightColor2 * LightBrightness2) * DiffuseProduct;
-    float Kd3 = max(dot(L3, N), 0.0);
+    float Kd3 = max(theta, 0.0);
     vec3  diffuse3 = Kd3 * (LightColor3 * LightBrightness3) * DiffuseProduct;
 
     float Ks = pow( max(dot(N, H), 0.0), Shininess );
     //vec3 specular = Ks * LightBrightness * SpecularProduct;
     float Ks2 = pow( max(dot(N, H2), 0.0), Shininess );
     //vec3 specular2 = Ks2 * LightBrightness2 * SpecularProduct;
-    float Ks3 = pow( max(dot(N, H3), 0.0), Shininess );
+    float Ks3 = pow( max(theta, 0.0), Shininess );
     vec3 specular3 = Ks3 * LightBrightness3 * SpecularProduct;
     
     vec3 brightness = vec3(5,5,5);
@@ -84,7 +87,6 @@ void main()
         specular3 = vec3(0.0, 0.0, 0.0);
     }
 
-
     //TASK F 
     float light_distance = 0.1 + length(Lvec);
     float light = 1.0/(1.0 + 1.0*length(Lvec) + light_distance);
@@ -92,18 +94,25 @@ void main()
 
     float light_distance_2 = 0.1 + length(Lvec2);
     float light2 = 1.0/(1.0 + 1.0*length(Lvec2) + light_distance_2);
-
+    vec4 colour_3;
     float light_distance_3 = 0.1 + length(Lvec3);
     float light3 = 1.0/(1.0 + 1.0*length(Lvec3) + light_distance_3);
+    
+    vec3 l3pos = LightPosition3.xyz;
 
 
-    // globalAmbient is independent of distance from the light source
+    if (theta > 0.9){
+       colour_3 = vec4(ambient3 + light_distance_3 * (diffuse3), 1.0) + light3;
+    }
+    else{
+        colour_3 = vec4(ambient3, 1.0)+light3;//maybe this decreases brightness of surrounding fragments
+    }
+
     vec3 globalAmbient = vec3(0.1, 0.1, 0.1);
-    //color.rgb = globalAmbient + ((ambient + diffuse) / light_distance) + specular2 + diffuse2;
-    color.rgb = globalAmbient + ((ambient + diffuse + specular) * light) + ambient2 + diffuse2 + (ambient3 + diffuse3) * light3;
+    color.rgb = globalAmbient + ((ambient + diffuse + specular) * light) + ambient2 + diffuse2;// + (ambient3 + diffuse3) * light3;
     color.a = 1.0;
-
-    gl_FragColor = color * texture2D(texture, texCoord * texScale);
+    
+    gl_FragColor = (color + colour_3) * texture2D(texture, texCoord * texScale);
 
 }
 
